d7d6a480636609ce82cf3bb8972ef5a63621b6c0
Kateryna Gunko <kateryna.gunko@fabware.com>
2016-05-20 16:52:19 +0300

    changing dropdown fields

--- client/src/app/common/directives/click-outside-element.directive.js
+++ client/src/app/common/directives/click-outside-element.directive.js
@@ -15,15 +15,16 @@
         element.on('click', function (event) {
           console.log('element clicked');
           clickInHandler(scope, {$event: event});
-          angular.element($window).on('click', function (event) {
-            if (element[0].contains(event.target)) return;
-            clickOutHandler(scope, {$event: event});
-            scope.$apply();
-            console.log('body clicked');
 
-            angular.element($window).off('click');
-          });
 
+            angular.element($window).on('click', function (event) {
+              if (element[0].contains(event.target)) return;
+              clickOutHandler(scope, {$event: event});
+              scope.$apply();
+              console.log('body clicked');
+
+              angular.element($window).off('click');
+            });
         });
       }
     }
